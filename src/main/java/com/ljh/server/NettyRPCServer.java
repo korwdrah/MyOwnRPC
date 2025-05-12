3b887c0de1766f520d54a899f3b2e1659d02bda8
korwdrah <2817659572@qq.com>
2025-05-12 16:24:39 +0800

    重构了RPC服务端的结构，实现了一个基于netty框架加速的服务端，server->initializer->handler initializer主要是初始化整个channel（入站事件->出站事件，然后解码成java类之后交给handler去反射调用） 自定义handler：反射调用方法处理请求

---
+++ src/main/java/com/ljh/server/NettyRPCServer.java
@@ -0,0 +1,44 @@
+package com.ljh.server;
+
+import com.ljh.server.provider.ServiceProvider;
+import com.ljh.server.serverInitialize.NettyServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyRPCServer implements RPCServer{
+    private ServiceProvider serviceProvider;
+    @Override
+    public void start(int port) {
+        //netty 服务线程组boss负责建立连接， work负责具体的请求 本质上是线程池
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup work = new NioEventLoopGroup();
+        System.out.println("netty服务端启动");
+        try{
+            //启动服务器
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            //初始化(指定处理请求的channel是服务器channel，同时指定Handler，也就是如何处理请求)
+            serverBootstrap.group(boss,work).channel(NioServerSocketChannel.class)
+                    .childHandler(new NettyServerInitializer(serviceProvider));
+
+            //绑定端口 同步阻塞（sync等待bind完成后才继续进行）
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            //死循环监听(中断不出现就一直阻塞主线程)
+            channelFuture.channel().closeFuture().sync();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ src/main/java/com/ljh/server/serverInitialize/NettyServerInitializer.java
@@ -0,0 +1,45 @@
+package com.ljh.server.serverInitialize;
+
+import com.ljh.server.provider.ServiceProvider;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.serialization.ClassResolver;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import lombok.AllArgsConstructor;
+
+//初始化channel的pipeline----即怎么接受请求 初始化TCP链接的SocketChannel
+//pipeline本质上是一个ChannelHandler的链表 处理链中流动的数据
+@AllArgsConstructor
+public class NettyServerInitializer extends ChannelInitializer<SocketChannel> {
+    private ServiceProvider serviceProvider;
+    //inbound和outbound保持相对顺序，内部的handler保持代码的顺序
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        //Inbound 流程
+        // 消息格式 [长度][消息体], 解决粘包问题 整个包的最大长度 长度字段的偏移量 长度字段大小（4字节---int) 需要调整的偏移量（不包含消息头的长度就要调整，0就代表就是消息体的长度） 从解码后的帧跳的字节数（跳过4个字节的长度字段）
+        // 解码消息
+        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,0,4,0,4));
+        //解码成Java类对象
+        pipeline.addLast(new ObjectDecoder(new ClassResolver() {
+            @Override
+            public Class<?> resolve(String className) throws ClassNotFoundException {
+                return Class.forName(className);
+            }
+        }));
+        //具体的调用方法
+        pipeline.addLast(new NettyRPCServerHandler(serviceProvider));
+
+        //outBound流程---编码 写入长度
+        // 编码器，长度字段，写入到前4个字节中 int类型
+        pipeline.addLast(new LengthFieldPrepender(4));
+
+        // 这里使用的还是java 序列化方式， netty的自带的解码编码支持传输这种结构
+        pipeline.addLast(new ObjectEncoder());
+
+    }
+}

b31d390be3094bf6c3ddf2b64913625ef9a9def0
korwdrah <2817659572@qq.com>
2025-05-14 20:54:09 +0800

    添加了服务端在zkp注册服务的方法

---
+++ src/main/java/com/ljh/utils/zkp/ZKServiceRegister.java
@@ -0,0 +1,66 @@
+package com.ljh.utils.zkp;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class ZKServiceRegister implements ServiceRegister{
+    //zk客户端
+    private CuratorFramework client;
+    // zookeeper根路径节点
+    private static final String ROOT_PATH = "MyRPC";
+    //初始化客户端
+    public ZKServiceRegister(){
+        // 指数时间重试
+        RetryPolicy policy = new ExponentialBackoffRetry(1000, 3);
+        //使用心跳监听 并建立于zkp的链接
+        client = CuratorFrameworkFactory.builder().connectString("127.0.0.1:2181").sessionTimeoutMs(40000).retryPolicy(policy).namespace(ROOT_PATH).build();
+        client.start();
+        System.out.println("zkp 链接成功");
+    }
+    //进行服务注册
+    @Override
+    public void register(String serviceName, InetSocketAddress serverAddress) {
+        try{
+            if(client.checkExists().forPath("/"+serviceName) == null){
+                //将服务创建成永久节点，提供者下线的时候只删除地址，不删除服务名
+                client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/"+serviceName);
+            }
+            //服务路径地址
+            String path = "/"+serviceName+"/"+getServiceAddress(serverAddress);
+            //创建临时节点
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path);
+        }
+        catch (Exception e){
+            System.out.println("服务已存在");
+        }
+    }
+    //根据服务名称返回地址
+    @Override
+    public InetSocketAddress serviceDiscovery(String serviceName) {
+        try {
+            List<String> paths = client.getChildren().forPath("/" + serviceName);
+            String addStr = paths.get(0);
+            return parseAddress(addStr);
+        }
+        catch (Exception e){
+            System.out.println("没有该服务");
+        }
+        return null;
+    }
+    //通过字符串解析服务地址
+    private InetSocketAddress parseAddress(String addStr) {
+        String[] split = addStr.split(":");
+        return new InetSocketAddress(split[0],Integer.parseInt(split[1]));
+    }
+
+    public String getServiceAddress(InetSocketAddress serverAddress){
+        //获取服务提供者的地址
+        return serverAddress.getHostName()+":"+serverAddress.getPort();
+    }
+}
